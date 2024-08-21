00f76ab482afdd2da7dbeb2130a89f8332a4336a
Marcel Colic <matschlgg@gmail.com>
2024-08-21 18:27:49 +0200

    Req03 and Req04 done
    Can now add Books

--- app/src/main/java/com/example/bookapp/Screen/AddBooks.kt
+++ app/src/main/java/com/example/bookapp/Screen/AddBooks.kt
@@ -1,20 +1,41 @@
 package com.example.bookapp.Screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bookapp.Widgets.BottomNavBar
 import com.example.bookapp.BottomNavigationItem
+import com.example.bookapp.models.Book
+import com.example.bookapp.models.addBook
+import com.example.bookapp.models.getBooks
+import com.example.bookapp.models.isbnChecker
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,17 +52,213 @@ fun AddBooks(navController: NavController){
             unselectedIcon = Icons.Outlined.Add
         )
     )
+    var title by remember {
+        mutableStateOf("")
+    }
+    var author by remember {
+        mutableStateOf("")
+    }
+    var release by remember {
+        mutableStateOf("")
+    }
+    var isbn1 by remember {
+        mutableStateOf("")
+    }
+    var isbn2 by remember {
+        mutableStateOf("")
+    }
+    var isbn3 by remember {
+        mutableStateOf("")
+    }
+    var isbn4 by remember {
+        mutableStateOf("")
+    }
+    var isbn5 by remember {
+        mutableStateOf("")
+    }
+    var error by remember {
+        mutableStateOf("AAAAAAAAAAA")
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             BottomNavBar(items, navController)
         }
+
     ) { innerPadding ->
-        Text(
-            text = "Add Books",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding),
+        ) {
+            Text(text = "ADD Books")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+
+                TextField(
+                    value = title,
+                    onValueChange = {title = it},
+                    label = {Text("Title")}
+                )
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+
+                TextField(
+                    value = author,
+                    onValueChange = {author = it},
+                    label = {Text("Author")}
+                )
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+
+                TextField(
+                    value = release,
+                    onValueChange = {release = it},
+                    label = {Text("Released")}
+                )
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                TextField(
+                    value = isbn1,
+                    onValueChange = { isbn1 = it },
+                    label = { Text("ISBN (1)") },
+                    modifier = Modifier.weight(1f) // verteilt den Platz gleichmäßig
+                )
+                Text("-", modifier = Modifier.padding(horizontal = 4.dp))
+                TextField(
+                    value = isbn2,
+                    onValueChange = { isbn2 = it },
+                    label = { Text("ISBN (2)") },
+                    modifier = Modifier.weight(1f)
+                )
+                Text("-", modifier = Modifier.padding(horizontal = 4.dp))
+                TextField(
+                    value = isbn3,
+                    onValueChange = { isbn3 = it },
+                    label = { Text("ISBN (3)") },
+                    modifier = Modifier.weight(1f)
+                )
+                Text("-", modifier = Modifier.padding(horizontal = 4.dp))
+                TextField(
+                    value = isbn4,
+                    onValueChange = { isbn4 = it },
+                    label = { Text("ISBN (4)") },
+                    modifier = Modifier.weight(1f)
+                )
+                Text("-", modifier = Modifier.padding(horizontal = 4.dp))
+                TextField(
+                    value = isbn5,
+                    onValueChange = { isbn5 = it },
+                    label = { Text("ISBN (5)") },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Button( onClick = {
+                    if (title.isNotBlank() && author.isNotBlank() && release.isNotBlank() && isbn1.isNotBlank() && isbn2.isNotBlank() && isbn3.isNotBlank() && isbn4.isNotBlank() && isbn5.isNotBlank()) { //checks if every TextField is filled out
+                        val releaseYear = release.toIntOrNull() //converts "release"-input into a number (int), when not possible into Null
+                        if (releaseYear != null) { //checks if the converted value is a number
+                            if (releaseYear in 0..2024) { //checks if release year value is between 0 and 2024
+                                if (isbn1.length + isbn2.length + isbn3.length + isbn4.length + isbn5.length == 13) { //checks if isbn input is exactly 13 digits long
+                                    if (isbn5.length == 1) {
+                                        val isbn1Check = isbn1.toLongOrNull() //converts "isbn"-input into a number (long) if isbn only contains numbers
+                                        val isbn2Check = isbn2.toLongOrNull()
+                                        val isbn3Check = isbn3.toLongOrNull()
+                                        val isbn4Check = isbn4.toLongOrNull()
+                                        val isbn5Check = isbn5.toLongOrNull()
+                                        if (isbn1Check != null && isbn2Check != null && isbn3Check != null && isbn4Check != null && isbn5Check != null) {
+                                            val isbnForCalc = isbn1 + isbn2 + isbn3 + isbn4 + isbn5
+                                            if(isbnChecker(isbnForCalc.toLong()))
+                                            {
+                                                val completeISBN = isbn1 + '-' + isbn2 + '-' + isbn3 + '-' + isbn4 + '-' + isbn5 //put all the digits from the isbn TextField into  valid isbn format
+                                                addBook(Book(title, author, release.toInt(), completeISBN))
+                                                title = ""
+                                                author = ""
+                                                release = ""
+                                                isbn1 = ""
+                                                isbn2 = ""
+                                                isbn3 = ""
+                                                isbn4 = ""
+                                                isbn5 = ""
+                                                error = ""
+                                            }
+                                            else {
+                                                error = "Invalid Input. The ISBN is not a valid ISBN."
+                                            }
+
+                                        } else {
+                                            error = "Invalid Input. The 'ISBN' field must only contain digits."
+                                        }
+                                    } else {
+                                        error = "Invalid Input. The last ISBN field must contain only one digit."
+                                    }
+                                } else {
+                                    error = "Invalid Input. The 'ISBN' field must contain exactly 13 digits."
+                                }
+                            } else {
+                                error = "Invalid Input. The 'Release' field must contain a number between 0 and 2024."
+                            }
+                        } else {
+                            error = "Invalid Input. The 'Release' field must contain a valid number."
+                        }
+                    } else {
+                        error = "Invalid Input. Every TextField must be filled out."
+                    }
+                }) {
+                    Text("Add Book")
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(text = error)
+            }
+        }
     }
 }
 
+
+@Preview
+@Composable
+fun AddBooksPreview()
+{
+
+}
--- app/src/main/java/com/example/bookapp/models/Book.kt
+++ app/src/main/java/com/example/bookapp/models/Book.kt
@@ -7,26 +7,63 @@ data class Book (
     val isbn: String
 )
     fun getBooks(): List<Book> {
-        return listOf(
-            Book(titel = "Die Tribute von Panem 1. Tödliche Spiele",
-                autor = "Suzanne Collins",
-                release = 2020,
-                isbn = "978-3-7891-2127-2"),
-            Book(titel = "Harry Potter und der Halbblutprinz",
-                autor = "J. K. Rowling",
-                release = 2018,
-                isbn = "978-3-551-55746-9"),
-            Book(titel = "1984",
-                autor = "George Orwell",
-                release = 2021,
-                isbn = "978-3-7306-0976-7"),
-            Book(titel = "Wir Kinder vom Bahnhof Zoo",
-                autor = "Kai Hermann",
-                release = 2017,
-                isbn = "978-3-551-31732-2"),
-            Book(titel = "Tschick",
-                autor = "Wolfgang Herrndorf",
-                release = 2013,
-                isbn = "978-3-944668-03-1"),
-        )
+        return bookList
     }
+
+fun addBook(book: Book){
+    bookList.add(book)
+}
+
+fun isbnChecker(isbn: Long): Boolean{
+    val firstDigit = isbn.toString().first().toString().toInt()
+    val secondDigit = isbn.toString()[1].toString().toInt()
+    val thirdDigit = isbn.toString()[2].toString().toInt()
+    val fourthDigit = isbn.toString()[3].toString().toInt()
+    val fifthDigit = isbn.toString()[4].toString().toInt()
+    val sixthDigit = isbn.toString()[5].toString().toInt()
+    val seventhDigit = isbn.toString()[6].toString().toInt()
+    val eighthDigit = isbn.toString()[7].toString().toInt()
+    val ninthDigit = isbn.toString()[8].toString().toInt()
+    val tenthDigit = isbn.toString()[9].toString().toInt()
+    val eleventhDigit = isbn.toString()[10].toString().toInt()
+    val twelveDigit = isbn.toString()[11].toString().toInt()
+    val checkSum = isbn.toString()[12].toString().toInt()
+
+    val isbnCalc = (firstDigit * 1 + secondDigit * 3 + thirdDigit * 1 + fourthDigit * 3 + fifthDigit * 1 + sixthDigit * 3 + seventhDigit * 1 + eighthDigit * 3 + ninthDigit * 1 + tenthDigit * 3 + eleventhDigit * 1 + twelveDigit * 3) % 10
+
+    if (isbnCalc == 0 && checkSum == 0)
+    {
+        return true
+    }
+
+    if (10 - isbnCalc == checkSum)
+    {
+        return true
+    }
+
+    return false
+}
+
+val bookList = mutableListOf(
+        Book(titel = "Die Tribute von Panem 1. Tödliche Spiele",
+            autor = "Suzanne Collins",
+            release = 2020,
+            isbn = "978-3-7891-2127-2"),
+        Book(titel = "Harry Potter und der Halbblutprinz",
+            autor = "J. K. Rowling",
+            release = 2018,
+            isbn = "978-3-551-55746-9"),
+        Book(titel = "1984",
+            autor = "George Orwell",
+            release = 2021,
+            isbn = "978-3-7306-0976-7"),
+        Book(titel = "Wir Kinder vom Bahnhof Zoo",
+            autor = "Kai Hermann",
+            release = 2017,
+            isbn = "978-3-551-31732-2"),
+        Book(titel = "Tschick",
+            autor = "Wolfgang Herrndorf",
+            release = 2013,
+            isbn = "978-3-944668-03-1")
+    )
+
